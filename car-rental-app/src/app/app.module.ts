8accc9dde8e6d5bef7606a270cfae1a7db9e47f2
Hugo <geadengeaden@gmail.com>
2022-03-11 15:05:01 +0100

    Add component contact

--- car-rental-app/src/app/app.module.ts
+++ car-rental-app/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu_component/menu/menu.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    HomepageComponent
+    HomepageComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
