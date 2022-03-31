ff4955ec1d093d96d1e8744f9890e9047aee238c
Matthias <matthias.fransioli@etu.univ-grenoble-alpes.fr>
2022-03-31 12:26:35 +0200

    feat(connexion): WIP : handle connexion using session id

--- car-rental-app/src/app/account/account.component.ts
+++ car-rental-app/src/app/account/account.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ConnexionService } from '../services/connexion.service';
 
 @Component({
   selector: 'app-account',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account.component.css']
 })
 export class AccountComponent implements OnInit {
-
-  constructor() { }
+user :any
+  constructor(private authenticationService : ConnexionService) { }
 
   ngOnInit(): void {
+    this.authenticationService.getAuthenticate('admin', 'admin').subscribe(data => {
+      this.user = data;
+      console.log("authentification works");
+      sessionStorage.setItem('id', this.user.id);
+      // TODO : gérer les connexions avec les sessionStorage (stocket l'id et si admin ou non)
+      });
   }
 
 }
