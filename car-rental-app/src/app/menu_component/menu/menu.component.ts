531e5b0a11ea06743880f353706296ac36507195
Matthias F <mauricefonzi@gmail.com>
2022-04-04 20:58:20 +0200

    feat(connexion): WIP add the name of the user in the nav bar when he is connected

--- car-rental-app/src/app/menu_component/menu/menu.component.ts
+++ car-rental-app/src/app/menu_component/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ConnexionService } from 'src/app/services/connexion.service';
 
 @Component({
   selector: 'app-menu',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-
-  constructor() { }
+  account:any;
+  isConnected:boolean = false;
+  theId:any;
+  constructor(private authenticationService : ConnexionService) { }
 
   ngOnInit(): void {
+    if (sessionStorage.getItem('id') !== undefined) {
+      this.isConnected = true;
+      this.account = sessionStorage.getItem("id");
+      this.authenticationService.getUserById(this.theId).subscribe(data => {
+        this.account = data;
+      });
+    }
   }
 
 }
