9c6c76e016a79209a67f110bce0ef83cbf6d71fe
Matthias F <mauricefonzi@gmail.com>
2022-04-10 01:02:36 +0200

    feat(connection): create and set up isAdmin? variable for a future use for the different menus according to the user connected

--- car-rental-app/src/app/menu_component/menu/menu.component.ts
+++ car-rental-app/src/app/menu_component/menu/menu.component.ts
@@ -9,20 +9,23 @@ import { ConnexionService } from 'src/app/services/connexion.service';
 export class MenuComponent implements OnInit {
   account:any;
   isConnected:boolean = false;
+  isAdmin:any = false;
   theId:any;
   logoutButtonText: string = 'Se déconnecter';
 
   constructor(private authenticationService : ConnexionService) { }
 
   ngOnInit(): void {
-    if (sessionStorage.getItem('id') !== undefined) {
+    if (sessionStorage.getItem('id') !== undefined && sessionStorage.getItem('admin') !== undefined) {
       this.isConnected = true;
+      // Cast the string value of the key 'admin' (from sessionStorage) into a boolean
+      this.isAdmin = sessionStorage.getItem("admin");
+      this.isAdmin = JSON.parse(this.isAdmin);
+
+      // Get the user's info from his/her id
       this.theId = sessionStorage.getItem("id");
       this.authenticationService.getUserById(this.theId).subscribe(data => {
         this.account = data;
-        console.log(this.account);
-        console.log(this.account.name);
-        console.log(this.account.firstName);
       });
     }
   }
@@ -32,5 +35,4 @@ export class MenuComponent implements OnInit {
     this.isConnected = false;
     window.location.reload();
   }
-
 }
