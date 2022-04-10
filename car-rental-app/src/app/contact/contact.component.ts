3c16b13d94bb3b3a262b4bd7a3329b8ef6d741e0
Matthias F <mauricefonzi@gmail.com>
2022-04-10 17:47:45 +0200

    feat(functionnal-contact): Display the mail, the subject, the country and the content of the contact form in console when sent

--- car-rental-app/src/app/contact/contact.component.ts
+++ car-rental-app/src/app/contact/contact.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  theEmail:string = "";
+  theSubject:string = "";
+  theCountry:string ="";
+  theMessage:string ="";
+
 
   constructor() { }
 
   ngOnInit(): void {
+    
+  }
+  sendMessage() {
+    console.log('Email: '+this.theEmail+' - Sujet: '+this.theSubject+' - Pays: '+this.theCountry+' - Message: '+this.theMessage);
   }
 
 }
