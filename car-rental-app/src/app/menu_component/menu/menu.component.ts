1127b3bd2be70334b24cefa7b0efd4c3e8de5c13
Matthias F <mauricefonzi@gmail.com>
2022-04-10 12:06:53 +0200

    feat(connection): add the acces to user or admin dashboard in the menu when the user is connected

--- car-rental-app/src/app/menu_component/menu/menu.component.ts
+++ car-rental-app/src/app/menu_component/menu/menu.component.ts
@@ -10,6 +10,7 @@ export class MenuComponent implements OnInit {
   account:any;
   isConnected:boolean = false;
   isAdmin:any = false;
+  isNotAdmin:any = false;
   theId:any;
   logoutButtonText: string = 'Se déconnecter';
 
@@ -21,6 +22,11 @@ export class MenuComponent implements OnInit {
       // Cast the string value of the key 'admin' (from sessionStorage) into a boolean
       this.isAdmin = sessionStorage.getItem("admin");
       this.isAdmin = JSON.parse(this.isAdmin);
+      // Change the value of isNotAdmin based on the value of isAdmin
+      // isNotAdmin is true when the user is a regular user
+      if (this.isConnected == true && this.isAdmin==false) {
+        this.isNotAdmin=true
+      }
 
       // Get the user's info from his/her id
       this.theId = sessionStorage.getItem("id");
@@ -30,6 +36,7 @@ export class MenuComponent implements OnInit {
     }
   }
 
+  //TODO: Factoriser le code pour placer la fonction de déconnexion dans le connexion.services
   logout() {
     sessionStorage.clear();
     this.isConnected = false;
